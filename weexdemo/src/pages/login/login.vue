39862a1030c78abb3a793c45a337db39ed396639
ShanatoYuuji <c1273099082@qq.com>
2018-01-15 23:47:09 +0900

    图片

--- weexdemo/src/pages/login/login.vue
+++ weexdemo/src/pages/login/login.vue
@@ -6,6 +6,9 @@
         <div style="margin-top:50px">
             <wxc-btn text="update" @wxcButtonClicked="update"></wxc-btn>
         </div>
+        <div  v-for="pic in pics"  v-bind:key="pic" class="divpic">
+            <image  :src="pic" class="pic"/>
+        </div>
     </div>
 </template>
 <script scoped>
@@ -17,7 +20,18 @@
         data(){
             return {
                logoUrl: 'http://img1.vued.vanthink.cn/vued08aa73a9ab65dcbd360ec54659ada97c.png',
-               target:'World'
+               target:'World',
+                pics:[
+                    "http://i2.bvimg.com/627877/9d8dba297168e98fs.jpg",
+                    "http://i2.bvimg.com/627877/c0e8b3b68107ec1ds.jpg",
+                    "http://i2.bvimg.com/627877/210154329d38fa68s.jpg",
+                    "http://i2.bvimg.com/627877/1b43f4ea16cb8cc6s.jpg",
+                    "http://i2.bvimg.com/627877/f311a9d3033f3bc7s.jpg",
+                    "http://i2.bvimg.com/627877/b979f66c7df9910bs.jpg",
+                    "http://i2.bvimg.com/627877/000e19bc38f124ces.jpg",
+                    "http://i2.bvimg.com/627877/bd3e1cdcf1bf564as.jpg",
+                    "http://i2.bvimg.com/627877/8a00412b94b10d3bs.jpg"
+                ]
             }
         },
         methods:{
@@ -34,6 +48,8 @@
   .title { padding-bottom: 40px; font-size: 48px;display: inline-block; }
   .logo { width: 360px; height: 156px; }
   .desc { padding-top: 20px; color:#888; font-size: 24px;display: inline-block;}
+  .divpic{padding: 10px 10px 10px 10px}
+  .pic{width:490px;height:320px}
 </style>
 
 
